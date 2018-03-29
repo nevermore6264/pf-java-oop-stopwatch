f31cbc6a414ccccf02ba2f02c74aeef718abc02b
nevermore6264 <nevermore6264@gmail.com>
2018-03-29 21:11:12 +0700

    [Bài tập] Xây dựng lớp StopWatch

---
+++ src/RandomNumber.java
@@ -0,0 +1,51 @@
+
+public class RandomNumber {
+    //Thuật toán sắp xếp tăng dần
+    public static void sortASC(int[] listNumber) {
+        int temp = listNumber[0];
+        for (int i = 0; i < listNumber.length - 1; i++) {
+            for (int j = i + 1; j < listNumber.length; j++) {
+                if (listNumber[i] > listNumber[j]) {
+                    temp = listNumber[j];
+                    listNumber[j] = listNumber[i];
+                    listNumber[i] = temp;
+                }
+            }
+        }
+    }
+
+    //Phương thức hiển thị danh sách
+    public static void showListNumber(int[] listNumber) {
+        for (int i = 0; i < listNumber.length; i++) {
+            System.out.print(listNumber[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        //Tạo mảng chứ 50 phần tử
+        int listNumber[] = new int[100000];
+
+        //Random 0 - 100000
+        for (int i = 0; i < 100000; i++) {
+            listNumber[i] = (int) Math.round(Math.random() * 100000);
+        }
+
+        //Sắp xếp và hiển thị
+        StopWatch stopwatch = new StopWatch();
+        stopwatch.start();
+
+        System.out.println("Started");
+
+        sortASC(listNumber);
+        showListNumber(listNumber);
+
+        System.out.println("Stopped");
+
+        stopwatch.stop();
+
+        double totalTime = (double) stopwatch.getElapsedTime() / 1000;
+        System.out.println("Total time is: " + totalTime + " seconds ");
+
+
+    }
+}
---
+++ src/StopWatch.java
@@ -0,0 +1,26 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+
+    }
+
+    public void start() {
+        this.startTime = new Date().getTime();
+
+    }
+
+    public void stop() {
+        this.endTime = new Date().getTime();
+
+    }
+
+    public long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
+
+}
